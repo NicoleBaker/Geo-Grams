b297621558ce69137ac75ac0fbe82ff4566c54c6
Brian Anderson <brianvanderson@gmail.com>
2012-10-20 23:41:35 -0700

    setup tw bootstrap, js and css

--- application/logs/2012/10/20.php
+++ application/logs/2012/10/20.php
@@ -14,3 +14,215 @@
 #1 /mnt/hgfs/geo/system/classes/kohana/request.php(202): Kohana_Cookie::get('session')
 #2 /mnt/hgfs/geo/index.php(108): Kohana_Request::factory()
 #3 {main}
+2012-10-20 22:57:46 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL welcome/index was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 87 ]
+2012-10-20 22:57:46 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL welcome/index was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 87 ]
+--
+#0 /mnt/hgfs/geo/system/classes/kohana/request/client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 /mnt/hgfs/geo/system/classes/kohana/request.php(1154): Kohana_Request_Client->execute(Object(Request))
+#2 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-10-20 23:16:12 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:16:12 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:18:58 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:18:58 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:18:58 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:18:58 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:02 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:02 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:02 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:02 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:10 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:10 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:10 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:10 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:24 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:24 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:24 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:24 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:28 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:28 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:28 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:28 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:33 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:33 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:33 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:33 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:51 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:51 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:19:51 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:19:51 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:20:31 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:20:31 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:20:31 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:20:31 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:01 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:01 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:01 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:01 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:26 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:26 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: mnt/hgfs/geo/application//assets/css/home.css ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:21:26 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:21:26 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:23:53 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:23:53 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:24:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:24:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:24:22 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:24:22 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:24:27 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:24:27 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:24:46 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:24:46 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:24:56 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:24:56 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:25:04 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:25:04 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:25:09 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:25:09 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:25:19 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:25:19 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:25:28 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:25:28 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:25:31 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:25:31 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:35:52 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.inview.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:35:52 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.inview.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:35:52 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.isotope.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:35:52 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.isotope.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:36:07 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.inview.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:36:07 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.inview.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:36:07 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.isotope.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:36:07 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/jquery.isotope.min.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:38:27 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:38:27 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:38:37 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:38:37 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-10-20 23:38:38 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+2012-10-20 23:38:38 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: assets/libs/bootstrap/js/bootstrap.js ~ SYSPATH/classes/kohana/request.php [ 1142 ]
+--
+#0 /mnt/hgfs/geo/index.php(109): Kohana_Request->execute()
+#1 {main}
---
+++ application/views/home.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en” lang="en” itemscope itemtype="http://schema.org/Organization">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Instagram Photos by Location | Geo-Grams</title>
+	<meta name="copyright" content="Geo-Grams">
+	<meta name="description" content="Beautiful Instagram photos of any Location.">
+	<meta property="og:title" content="Find beautiful Instagram " >
+	<meta property="og:type" content="Website" >
+	<meta property="og:url" content="geo-grams.com" >
+	<meta property="og:description" content="Find beautiful Instagram photos of any location." >
+	<meta property="og:image" content="" >
+	<meta property="og:site_name" content="Geo-Grams" >
+	<link rel='SHORTCUT ICON' href="" type='image/x-icon' />
+	<link rel='apple-touch-icon' href="" />
+	<meta name="author" content="Brian Anderson, Nicole Baker">
+	
+	<?php echo html::style('/assets/libs/bootstrap/css/bootstrap.min.css') ?>
+	<?php echo html::style("/assets/css/home.css") ?>
+	<?php echo html::style('/assets/libs/bootstrap/css/bootstrap-responsive.min.css') ?>
+	
+</head>
+<body>
+	
+	<h4>hello geo-grams world</h4>
+	
+</body>
+</html>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<?php echo html::script('/assets/libs/bootstrap/js/bootstrap.min.js') ?>
+<?php echo html::script('/assets/libs/jquery.isotope.min.js') ?>
+<?php echo html::script('/assets/js/home.js') ?>
